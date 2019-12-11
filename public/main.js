1283d793aeb78dea1c5625ced07127090a782357
Darian Jarvis <djarvis@nyc.yearup.org>
2019-12-11 12:43:36 -0500

    updated landing page style and results page scrolls to the first item.

---
+++ public/main.js
@@ -0,0 +1,9 @@
+
+
+
+window.onload = function scroll() {
+  console.log('testing');
+  firstResult = document.getElementsByClassName('book-result');
+  firstResult[0].scrollIntoView()
+// firstResult.scrollIntoView({behavior: "smooth"})
+}
